61863fbbf63392d29a13b2f1ec918fd877fbea70
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2024-04-11 23:41:22 -0300

    iframe2

--- src/sections/Schedule.jsx
+++ src/sections/Schedule.jsx
@@ -29,7 +29,10 @@ function Schedule() {
       <h3 className={styles.subtitle}>Lugar</h3>
       <p className={styles.text}>Cassano Eventos</p>
       <p className={styles.text}>Río Negro 1270, El Palomar</p>
-      <Button text={howToArriveText} onClick={handleClickMap} />
+      <a href="https://www.google.com/maps/place/DAR,+R%C3%ADo+Negro+1270,+B1685+El+Palomar,+Provincia+de+Buenos+Aires/@-34.615663,-58.5856481,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb869f8d2d70f:0x634aed80593af51a!8m2!3d-34.615663!4d-58.5830732!16s%2Fg%2F11c5dv35zp?entry=ttu" target="_blank" className={styles.href} rel="noreferrer">
+
+      <Button text={howToArriveText} func={handleClickMap} />
+</a>
       <p className={styles.text}>Te espero para compartir conmigo</p>
       <a
         className={styles.ahref}
